8d82ac76529ee8d3ecec3e4c76f03389d9f5593b
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-27 14:53:32 +0900

    기본 게시판

--- board/views.py
+++ board/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
-
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # 게시판 목록 출력
 def index(request):
@@ -18,12 +20,14 @@ def detail(request, question_id):
     return render(request, 'board/question_detail.html', context)
 
 # 답변 등록
+@login_required(login_url='user:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -34,11 +38,13 @@ def answer_create(request, question_id):
     return render(request, 'board/question_detail.html', context)
 
 # 질문 등록
+@login_required(login_url='user:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST) # request.POST에는 화면에서 사용자가 입력한 내용들이 담겨있다.
         if form.is_valid(): # 폼이 유효하다면
             question = form.save(commit=False) # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now() # 실제 저장을 위해 작성일시를 설정한다.
             question.save()  # 데이터를 실제로 저장한다.
             return redirect('board:index')
@@ -47,4 +53,73 @@ def question_create(request):
     context = {'form': form}
     return render(request, 'board/question_form.html', context)
 
+# 페이징
+def index(request):
+    #입력인자
+    page = request.GET.get('page', '1')  # 페이지
+    #조회
+    question_list = Question.objects.order_by('-create_date')
+    #페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj} # question_list는 페이징 객체(page_obj)
+    return render(request, 'board/question_list.html', context)
+
+# 질문 수정
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'board/question_form.html', context)
+
+# 질문 삭제
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('board:detail', question_id=question.id)
+    question.delete()
+    return redirect('board:index')
+
+# 답변 수정
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('board:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'board/answer_form.html', context)
+
+# 답변 삭제
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('board:detail', question_id=answer.question.id)
 
--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from board import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('user/', include('user.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
